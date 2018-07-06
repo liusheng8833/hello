e8c25ca9bcc8ef8c7126b23f82884fbca62f10cf
liusheng8833 <sheng@localhost.localdomain>
2018-07-06 11:39:18 -0400

    initial commit

--- hello.go
+++ hello.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/liusheng8833/stringutil"
+)
 
 func main() {
-	fmt.Printf("Hello, world from liusheng8833.\n")
+	fmt.Printf(stringutil.Reverse("!dlroW weN oG ,olleH"))
 }
